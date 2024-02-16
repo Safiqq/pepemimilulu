c22d93dc07e76d039157213f465410b8db816fa0
safiqq <szarifin20041@gmail.com>
2024-02-16 15:51:56 +0700

    feat: save c.hasil image as grayscale

--- getchasil.py
+++ getchasil.py
@@ -2,6 +2,8 @@ import json
 import os
 import sys
 import requests
+import cv2 as cv
+import numpy as np
 
 if not os.path.exists('data'): 
     sys.exit('No data found')
@@ -9,12 +11,13 @@ if not os.path.exists('data'):
 if not os.path.exists('assets'): 
     os.makedirs('assets')
 
-for root, dirs, files in os.walk('data/provinsi'):
+for root, dirs, filenames in os.walk('data/provinsi'):
     if root.count('/') == 5:
-        for file in files:
-            with open(f'{root}/{file}', 'r', encoding='utf-8') as f:
+        for filename in filenames:
+            with open(f'{root}/{filename}', 'r', encoding='utf-8') as f:
                 img_url = json.loads(f.read())['images'][1]
                 if img_url is not None:
-                    img_data = requests.get(img_url).content
-                    with open(f'assets/{file.replace("json", "jpg")}', 'wb') as img:
-                        img.write(img_data)
+                    response = requests.get(img_url)
+                    img_data = np.frombuffer(response.content, dtype=np.uint8)
+                    img = cv.imdecode(img_data, cv.IMREAD_GRAYSCALE)
+                    cv.imwrite(f'assets/{filename.replace("json", "jpg")}', img)
