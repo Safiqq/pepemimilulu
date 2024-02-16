6b475b761c4910adbc207cb92ea2628376d9a5ae
safiqq <szarifin20041@gmail.com>
2024-02-16 13:50:39 +0700

    feat: change file name to scrape and change tpsbelumadachasil to set (no dupl)

--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import os 
+import os
 
 if not os.path.exists('data'): 
     os.makedirs('data')
@@ -10,7 +10,7 @@ WILAYAH_URL = BASE_URL + "/wilayah"
 PPWP_URL = WILAYAH_URL + "/pemilu/ppwp"
 HHCW_PPWP_URL = BASE_URL + "/pemilu/hhcw/ppwp"
 
-tpsbelumadachasil = []
+tpsbelumadachasil = set()
 
 presiden = requests.get(f'{BASE_URL}/pemilu/ppwp.json').json()
 with open('data/presiden.json', 'w', encoding='utf-8') as f:
@@ -23,7 +23,7 @@ if not os.path.exists('data/provinsi'):
     os.makedirs('data/provinsi')
 if os.path.exists('data/tpsbelumadachasil.json'): 
     f = open('data/tpsbelumadachasil.json', 'r', encoding='utf-8')
-    tpsbelumadachasil = json.loads(f.read())
+    tpsbelumadachasil = set(json.loads(f.read()))
     f.close()
 
 for provinsi_ in provinsi:
@@ -56,5 +56,5 @@ for provinsi_ in provinsi:
                         json.dump(chasil, f, ensure_ascii=False, indent=4)
                     if chasil["images"][1] == None:
                         with open('data/tpsbelumadachasil.json', 'w', encoding='utf-8') as f:
-                            tpsbelumadachasil.append(f'{HHCW_PPWP_URL}/{provinsi_["kode"]}/{kotakab_["kode"]}/{kecamatan_["kode"]}/{desakel_["kode"]}/{tps_["kode"]}.json')
-                            json.dump(tpsbelumadachasil, f, ensure_ascii=False, indent=4)
+                            tpsbelumadachasil.add(f'{HHCW_PPWP_URL}/{provinsi_["kode"]}/{kotakab_["kode"]}/{kecamatan_["kode"]}/{desakel_["kode"]}/{tps_["kode"]}.json')
+                            json.dump(list(tpsbelumadachasil), f, ensure_ascii=False, indent=4)
